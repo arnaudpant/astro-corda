71bf91eefefebab7c210e24ecbd3c06d34ecf880
macbook kal <arnaud.pantenier@gmail.com>
2025-06-10 22:36:16 +0200

    ADD SectionCoachong

---
+++ src/data/cardsCoaching.ts
@@ -0,0 +1,35 @@
+export type CardCoachingInfos = {
+    imgSrc: string,
+    imgAlt: string,
+    title: string,
+    intro: string,
+    liArr: string[],
+    link: string
+}
+
+export const CardsCoaching: CardCoachingInfos[] = [
+    {
+        imgSrc: '/coaching/card-nutrition.jpg',
+        imgAlt: 'coaching nutrition',
+        title: 'NUTRITION',
+        intro: 'Vous voulez:',
+        liArr: ['▸ Perdre du poids ou en reprendre', '▸ Rééquilibrer votre flore intestinale', '▸ Retrouver la santé'],
+        link: "/coaching-nutrition"
+    },
+    {
+        imgSrc: '/coaching/card-stress.jpg',
+        imgAlt: 'coaching gestion des stress',
+        title: 'GESTION des STRESS et des EMOTIONS',
+        intro: 'Vous souffrez de:',
+        liArr: ['▸ Anxiétés, angoisses, insomnies', '▸ Migraines, acouphènes', '▸ Ulcère, contractures, spasmophilie...'],
+        link: "/coaching-gestion-stress"
+    },
+    {
+        imgSrc: '/coaching/card-sport.jpg',
+        imgAlt: 'coaching sport',
+        title: 'SPORT SANTÉ',
+        intro: 'Vous sortez:',
+        liArr: ['▸ D\’une longue période sans activité physique', '▸ D\'une opération', '▸ D\'un burn-out, maladie, autre ...'],
+        link: "/coaching-sport"
+    },
+]
