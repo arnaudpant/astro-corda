1f4aa73e0cb474b57c6c97afa4772e6f3cc780dd
macbook kal <arnaud.pantenier@gmail.com>
2025-06-11 22:05:37 +0200

    FIX bug affichage cardCoaching

--- src/data/cardsCoaching.ts
+++ src/data/cardsCoaching.ts
@@ -1,35 +1,8 @@
 export type CardCoachingInfos = {
     imagePath: string,
     altText: string,
-    title: string,
-    intro: string,
+    titleCard: string,
+    introCard: string,
     liArr: string[],
-    link: string
+    linkCard: string
 }
-
-export const CardsCoaching: CardCoachingInfos[] = [
-    {
-        imagePath: '/src/assets/coaching/card-nutrition.jpg',
-        altText: 'coaching nutrition',
-        title: 'NUTRITION',
-        intro: 'Vous voulez:',
-        liArr: ['▸ Perdre du poids ou en reprendre', '▸ Rééquilibrer votre flore intestinale', '▸ Retrouver la santé'],
-        link: "/coaching-nutrition"
-    },
-    {
-        imagePath: '/src/assets/coaching/card-stress.jpg',
-        altText: 'coaching gestion des stress',
-        title: 'GESTION des STRESS et des EMOTIONS',
-        intro: 'Vous souffrez de:',
-        liArr: ['▸ Anxiétés, angoisses, insomnies', '▸ Migraines, acouphènes', '▸ Ulcère, contractures, spasmophilie...'],
-        link: "/coaching-gestion-stress"
-    },
-    {
-        imagePath: '/src/assets/coaching/card-sport.jpg',
-        altText: 'coaching sport',
-        title: 'SPORT SANTÉ',
-        intro: 'Vous sortez:',
-        liArr: ['▸ D\’une longue période sans activité physique', '▸ D\'une opération', '▸ D\'un burn-out, maladie, autre ...'],
-        link: "/coaching-sport"
-    },
-]
