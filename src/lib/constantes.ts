8dc96f8d5f1dc8e080885a038614fb4a8c24ac0e
macbook kal <arnaud.pantenier@gmail.com>
2025-06-16 14:31:35 +0200

    ADD CGU

---
+++ src/lib/constantes.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = "Naturopathe sophrologue coach sportif 26300 Marches"
+export const SITE_DESCRIPTION = "Retrouvez votre poids de forme, gérez vos stress et retrouvez une activité physique avec Joelle Corda, naturopathe, sophrologue. 26300 Marche"
+
+export const CGU_TITLE = "Conditions générales d'utilisation"
+export const CGU_DESCRIPTION = "Conditions générales d'utilisation pour le site de Joelle Corda, Naturopathe sophrologue et coach sportif à Marches (26000)"
